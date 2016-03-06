b2a10065cc6548bcb347899dd818cc7f30497569
dburt4 <daniel.burt99@gmail.com>
2016-03-06 02:50:50 -0700

    Added files via upload

--- portScan2.py
+++ portScan2.py
@@ -5,10 +5,6 @@ import subprocess
 #USAGE: python portScan2.py -h <fileName> -p { <portNum> | <portNum-portNum> }
 
 if __name__ == '__main__':
-    #target = raw_input('Enter host to scan: ')
-    #targetIP = gethostbyname(sys.argv[2])
-    #print 'Starting scan on host ', targetIP
-
     #40 - working
     #5 read hosts from file
     #10 multiple ports to scan
@@ -19,6 +15,7 @@ if __name__ == '__main__':
     if len(sys.argv) == 6 and sys.argv[5] == '-udp':
         connectionType = SOCK_DGRAM
     print connectionType
+
     #get the port range:
     if "-" in sys.argv[4]:
         lowerPortRange = sys.argv[4].split("-")[0]
@@ -30,24 +27,25 @@ if __name__ == '__main__':
     out = file('report.html', 'w')
     print "Port scan results: (output also reported in HTML page called report.html)"
     print >> out, "<html><head><title>Port scan report</title></head><body>"
+
+    #Open file:
     with open(sys.argv[2]) as fileName:
         print >> out, "<div class='main' style='width: 600px; margin: 0 auto'>"
+
+        #For each host in the file:
         for host in fileName:
             print >> out, "<h2> %s open ports:</h2><ul>" %(host)
             print "\n\nHOST: %s" %(host)
+
+            #for each port in  the range:
             for portNum in range(int(lowerPortRange), int(upperPortRange)+1):
                 s = socket(AF_INET, connectionType)
                 try:
-
-                    #orig_stdout = sys.stdout
-                    #sys.stdout = out
                     result = s.connect_ex((host, portNum))
                     if(result == 0):
                         print >> out, '<li> %d: </li>' % (portNum,)
                         print 'Port %d: OPEN' % (portNum,)
                     s.close()
-                    #sys.stdout = orig_stdout
-
 
                 except gaierror:
                     print "could not connect to host"
